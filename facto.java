2b298f81355d6dc4f925807b0c0519529a6317a8
Guix98 <guimega.gh@gmail.com>
2019-08-29 21:29:09 -0400

    añadiendo primo

--- facto.java
+++ facto.java
@@ -2,6 +2,7 @@ class Main {
   public static void main(String[] args) {
     facto(5);
   }
+
   public static void facto(double n){
     double factorial = 1;
         while ( n!=0) {
@@ -9,4 +10,19 @@ class Main {
       n--;
     }
   System.out.println(factorial);}
+
+public static void primo(int n){
+       int a=0;    
+         for(int i=1;i<(n+1);i++){
+         if(n%i==0){
+             a++;
+            }
+         }
+         if(a!=2){
+              System.out.println("No es primo");
+            }else{
+                System.out.println("Es primo");
+         }
+     }
+}
 }
